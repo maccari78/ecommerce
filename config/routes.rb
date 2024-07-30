e682cb25a62b7e6af8e150558b8044f87475e7b5
Danilo Maccari <maccari78@gmail.com>
2024-07-30 19:36:35 -0300

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-
-  devise_for :admins, controllers: { sessions: 'admin/sessions' }
+  devise_for :admins
   resources :products
   resource :admin, only: [:show], controller: :admin
 
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "products#index"
-
-  delete 'logout', to: 'sessions#destroy'
 end
